2f8ecf92839ab338b3e580f9ff2d261f11b297ab
Rafa-Fandinho <rafael.fandino.ruanova@rai.usc.es>
2026-03-13 01:24:47 +0000

    Move it
    Move it

--- bookstore/src/main/kotlin/pt/unl/fct/iadi/bookstore/BookstoreApplication.kt
+++ bookstore/src/main/kotlin/pt/unl/fct/iadi/bookstore/BookstoreApplication.kt
@@ -1,4 +1,4 @@
-package pt.unl.fct.iadi.bookstore
+package org.dropproject.samples.bookstore
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class BookstoreApplication
 
 fun main(args: Array<String>) {
-	runApplication<BookstoreApplication>(*args)
+    runApplication<BookstoreApplication>(*args)
 }
